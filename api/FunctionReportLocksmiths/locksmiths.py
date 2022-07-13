110a58fa8036f5b913a25e0d0f593073e6fa86f8
felipemeloc <felipemeloc@gmail.com>
2022-07-13 17:55:59 +0100

    Code to lunch the locksmith report, added new blob module to download or upload files from the Azure containers

---
+++ api/FunctionReportLocksmiths/locksmiths.py
@@ -0,0 +1,124 @@
+"""locksmiths.py
+
+Locksmiths push bot for the hourly report of locksmiths jobs
+The script preloads the txt files with each query. 
+It uses the functions of the src.utils_bot module to modify the result of the queries. 
+With the information, it generates the report and sends it to Telegram using the src.bot module.
+
+
+The script needs the installation of the following packages:
+* os: For path management and directory creation
+* pandas: Return a DataFrame object
+* dotenv: Load environment variables
+* logging: Log management
+
+This script uses the following custom modules:
+* src: Adapt the format of the data to be printed on Telegram
+
+"""
+import os
+import pandas as pd
+from .src import db
+from .src import bot
+from .src import utils_bot
+from .src import azure_blob as blob
+from dotenv import load_dotenv
+import logging
+
+# Load environment variables
+load_dotenv()
+# Define project main path
+MAIN_FOLDER = os.getenv('MAIN_PATH')
+
+# LOG File save
+logging.basicConfig(
+    format= '%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+    )
+
+REPORT_IMAGE = os.getenv('REPORT_IMAGE')
+
+################################## Query Load #####################################
+
+query_path = os.path.join(MAIN_FOLDER, 'queries')
+
+LS_total_pending_jobs_by_locksmith_day = open(os.path.join(query_path,
+                    'LS_total_pending_jobs_by_locksmith_day.sql'), 'r').read()
+
+LS_total_pending_jobs_day = open(os.path.join(query_path,
+                    'LS_total_pending_jobs_day.sql'), 'r').read()
+
+LS_total_completed_jobs_day = open(os.path.join(query_path,
+                    'LS_total_completed_jobs_day.sql'), 'r').read()
+
+LS_total_revenue_day = open(os.path.join(query_path,
+                    'LS_total_revenue_day.sql'), 'r').read()
+
+LS_selected_vs_invoice_locksmiths = open(os.path.join(query_path,
+                    'LS_selected_vs_invoice_locksmiths.sql'), 'r').read()
+
+
+def main(GROUP_ID):
+    """Main function, it is in charge of:
+    * Query the database
+    * Transform dataframes to strings
+    * Assemble report
+    * Send report to Telegram
+    """    
+    date =  pd.Timestamp.now(tz="Europe/London").strftime('%A, %d %B %H:%M')
+    # Today's Pending Jobs
+    locksmiths_jobs_pending = utils_bot.df_locksmith_to_str( db.sql_to_df(LS_total_pending_jobs_by_locksmith_day))
+    total_jobs_pending = utils_bot.trans_one_row(db.sql_to_df(LS_total_pending_jobs_day))
+
+    # Today's Completed Jobs
+    total_revenue = utils_bot.trans_one_row(db.sql_to_df(LS_total_revenue_day), money=True)
+    total_jobs_completed = utils_bot.trans_one_row(db.sql_to_df(LS_total_completed_jobs_day))
+    
+    # Not maching locksmiths
+    selected_vs_invoice_locksmiths = utils_bot.selected_vs_invoice_locksmiths(db.sql_to_df(LS_selected_vs_invoice_locksmiths))
+    
+    message = f"""{date}\n
+*TODAY'S PENDING JOBS:*\n
+{locksmiths_jobs_pending}\n
+{total_jobs_pending}\n
+*TODAY'S COMPLETED JOBS:*\n
+{total_revenue}
+{total_jobs_completed}\n
+"""
+    if selected_vs_invoice_locksmiths:
+        message = message + f"""*DISCREPANCY FOUND:*\n
+{selected_vs_invoice_locksmiths}"""
+    logging.info(message)
+    bot.send_message(GROUP_ID, message)
+    
+    if blob.exists(REPORT_IMAGE):
+        bot.send_photo(GROUP_ID, REPORT_IMAGE, 'Completed jobs summary')
+        
+def send_locksmiths_report(special=False, test=False):
+    if test:
+        # For TEST
+        GROUP_ID = os.getenv('TEST_GROUP')
+    else:
+        # Define chat id
+        GROUP_ID = os.getenv('LOCKSMITHS_GROUP')
+    
+    try:
+        NOW = pd.Timestamp.now(tz="Europe/London")
+        hour = NOW.hour
+        logging.info('Bot online')
+        # Time validation to check if the hour is between 6 and 21
+        if (hour >= 6 and hour <= 21) or special:
+            main(GROUP_ID)
+            logging.info('Process Successful')
+        else:
+            logging.info('Execution after hours')
+        # if blob.exists(REPORT_IMAGE):
+        #     blob.delete_blob(REPORT_IMAGE)
+        #     logging.info('Locksmiths report image deleted')
+    except Exception as e:
+        logging.exception(e)
+
+if __name__ == '__main__':
+    send_locksmiths_report(
+        special= True,
+        test= True)
---
+++ api/FunctionReportLocksmiths/src/azure_blob.py
@@ -0,0 +1,99 @@
+"""azure_blob.py
+This is a custom module to download azure files to
+local or upload local files to azure
+This module needs the installation of the following packages:
+* os: For path management and directory creation
+* dotenv: load environment variables
+* azure.storage.blob:
+Contains the following function:
+
+* download: Function to download azure files to local. use:
+    import azure_blob as blob
+    blob.download(cloud_file_name= 'FILE_TO_BE_DOWLOAD',
+                local_file_name= 'FILE_DOWLOAD_NAME')
+    
+* upload: Function to upload local files to azure. use:
+    import azure_blob as blob
+    blob.upload(local_file_name='FILE_TO_BE_UPLOAD',
+                cloud_file_name='FINAL_UPLOAD_NAME')
+"""
+
+import os
+from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient, _container_client
+
+load_dotenv()
+
+def init_blob()->_container_client.ContainerClient:
+    """Connection blob function, it is used to download azure files to
+    local or upload local files to azure.
+
+    Returns:
+        _container_client.ContainerClient: Azure connection blob
+    """    
+    conn_str = os.getenv('BLOB_CONN_STR')
+    container = os.getenv('CONTAINER')
+    blob = BlobServiceClient.from_connection_string(
+        conn_str
+        ).get_container_client(
+            container
+            )
+    return blob
+
+def blob_exists(blob_path:str)->bool:
+    """Function to check if a file exists in Azure
+
+    Args:
+        blob_path (str): Path to an Azure file
+
+    Returns:
+        bool: Boolean if the file exists or not in Azure
+    """    
+    blob = init_blob()
+    return blob.get_blob_client(blob_path).exists()
+
+def download(cloud_file_name:str, local_file_name:str=None, as_bytes:bool=False):
+    """Function to download azure files to local.
+    It needs the target file
+
+    Args:
+        cloud_file_name (str): Target file in azure
+        local_file_name (str, optional): The final path where
+            the file will be after download. Defaults to None (same as target).
+    """    
+    blob = init_blob()
+    if not local_file_name:
+        local_file_name = cloud_file_name
+    if blob_exists(cloud_file_name):
+        blob_data = blob.download_blob(cloud_file_name)
+        if as_bytes:
+            return blob_data.content_as_bytes()
+        else:
+            with open(local_file_name, 'wb') as f:
+                blob_data.readinto(f)
+    
+def upload(local_file_name:str, cloud_file_name:str=None):
+    """Function to upload local files to azure.
+    It needs the target file
+
+    Args:
+        local_file_name (str):Target file in local
+        cloud_file_name (str, optional): The final path where
+            the file will be after upload. Defaults to None (same as target).
+    """
+    blob = init_blob()
+    if not cloud_file_name:
+        cloud_file_name = local_file_name
+    if os.path.exists(local_file_name):
+        with open(local_file_name, 'rb') as f:
+            blob.upload_blob(cloud_file_name, f)
+
+def delete_blob(blob_path:str)->None:
+    """Function to delete a Azure file
+
+    Args:
+        blob_path (str): Path to the blob to be deleted
+    """
+    blob = init_blob()
+    if blob_exists(blob_path):
+        blob.delete_blob(blob_path)
